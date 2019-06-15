3bd87d5386763e75e5b7b8c42be915327cf99a2d
well <wellmend0@gmail.com>
2019-06-15 01:04:40 -0300

    add current code

---
+++ src/request.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from notify_run import Notify
+import time
+import yaml
+
+def openQacademico(driver):
+    driver.get('https://qacademico.ifce.edu.br/qacademico/alunos')
+
+    username = driver.find_element_by_id('txtLogin')
+    password = driver.find_element_by_id('txtSenha')
+
+    with open("resources/credentials.yml", 'r') as stream:
+        try:
+            yamlData = yaml.safe_load(stream)
+            username.send_keys(yamlData['credentials']['username'])
+            password.send_keys(yamlData['credentials']['password'])
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    loginBtn = driver.find_element_by_id("btnOk")
+    loginBtn.click()
+
+    driver.get('https://qacademico.ifce.edu.br/qacademico/index.asp?t=2071')
+
+    return driver
+
+def updateQacademico(driver):
+    driver.refresh()
+    if(driver.current_url != 'https://qacademico.ifce.edu.br/qacademico/index.asp?t=2071'):
+        driver = openQacademico(driver)
+    return driver.page_source.count('Nota:')
+
+opt = Options()
+opt.set_headless(True)
+driver = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver', options=opt)
+notify = Notify()
+
+openQacademico(driver)
+lastValue = updateQacademico(driver)
+aliveTrigger = 1
+thresh_time  = 60
+
+while True:
+    value = updateQacademico(driver)
+    strNota = 'NOTA NO ACADEMICO'
+    strAlive = 'NOTAS POSTADAS: ' + str(value)
+
+    if(lastValue != value):
+        print(strNota)
+        notify.send(strNota)
+        lastValue=value
+        aliveTrigger = 1
+
+    if(aliveTrigger==thresh_time):
+        print(strAlive)
+        notify.send(strAlive)
+        aliveTrigger = 1
+
+    aliveTrigger+=1
+    time.sleep(1)
