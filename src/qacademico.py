cb3012bbdb133ef0f6e3a49b4c8c9cca59a55bf0
well <wellmend0@gmail.com>
2019-06-15 11:08:33 -0300

    interpreter html and get all grades

--- src/qacademico.py
+++ src/qacademico.py
@@ -18,8 +18,59 @@ def getCountGrades(driver):
     return grades
 
 def getGrades(driver):
-    grades = driver.page_source.count('Nota:')
-    return grades
+    elementsCourse = driver.find_elements_by_tag_name('strong')
+    elementsAllContent = driver.find_elements_by_class_name('conteudoTexto')
+    grades = []
+    enablerList = False
+    enabler = False
+    selector = 0
+
+    for e_course in elementsCourse:
+        for e_all in elementsAllContent:
+            if(e_all.text.count('-')==3):
+                if(e_course.text == e_all.text):
+                    str_txt = e_all.text
+                    begin = str_txt.find( '-',str_txt.find( '-' )+1 )+2
+                    end = str_txt.find('-', begin)-1
+                    #print(str_txt)
+                    grades.append([str_txt[begin:end]])
+                    enablerList = True
+                else:
+                    enablerList = False
+            else:
+                if(enablerList):
+                    if(e_all.text[:2] == 'N1'):
+                        selector = 1
+                        enabler = True
+                    elif(e_all.text[:2] == 'N2'):
+                        selector = 2
+                        enabler = True
+                    elif(enabler):
+                        if(selector==1):
+                            idx = e_all.text.find('Nota:')
+                            if(idx==-1):
+                                enabler = False
+                            else:
+                                #print('N1: ', e_all.text[idx:idx+10])
+                                grades[len(grades)-1].append('N1: ' + e_all.text[idx:idx+10])
+                        elif(selector==2):
+                            idx = e_all.text.find('Nota:')
+                            if(idx==-1):
+                                enabler = False
+                            else:
+                                #print('N2: ', e_all.text[idx:idx+10])
+                                grades[len(grades)-1].append('N1: ' + e_all.text[idx:idx+10])
+
+    gradesStr = ''
+    for grade in grades:
+        for i in grade:
+            if(i==grade[0]):
+                gradesStr += i + ':\n'
+            else:
+                gradesStr+= '     ' + i + '\n'
+        gradesStr+='\n'
+        
+    return gradesStr
 
 def openPage(driver):
     # Get into qacademico 'alunos login' page
